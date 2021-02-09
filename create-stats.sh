91825b7d202cb9b102cb3900ee7e866204fc4422
kiwiepic <grpiccoli@gmail.com>
2021-02-09 20:15:47 +1300

    added create-stats

---
+++ create-stats.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cp -R /usr/local/bin/assembly-stats-17.02 .
+mkdir -p json
+cmd="cat /usr/local/bin/assembly-stats-17.02/assembly-stats.html"
+for (( i= 0; i <= $#; i++))
+do
+    name=`basename $1`
+    name=${name//./_}
+    echo "var $name = " > json/$name.json
+    asm2stats.pl $1 >> json/$name.json
+    echo "localStorage.setItem('$name',JSON.stringify($name))" >> json/$name.json
+    cmd="$cmd | sed -r 's%<!--add_jsons_here-->%a <script type=\"text/javascript\" src=\"json/$name.json\"></script>%'"
+    shift
+done
+eval $cmd
